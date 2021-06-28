c65de0185991811683595ed1815a658277a4831a
ghdcksgml1 <ghdcksgml2@naver.com>
2021-06-28 19:54:20 +0900

    V.1.9

--- todo.js
+++ todo.js
@@ -30,9 +30,9 @@ function paintToDo(text){
 
     delBtn.innerText = "❌"; // button의 value값
     delBtn.addEventListener("click",deleteToDo); // 이벤트 리스너
-    span.innerText = text; //
-    li.appendChild(span);
+    span.innerText = ` ${text}`; //
     li.appendChild(delBtn);
+    li.appendChild(span);
     li.id = newId;
     toDoList.appendChild(li);
     const toDoObj = {
@@ -46,6 +46,7 @@ function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
+    toDoInput.value = "";
 }
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
